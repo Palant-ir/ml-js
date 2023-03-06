4a2102f7e0dc976e0d602d3438805ec6dafad968
han <johnmfdoe42@gmail.com>
2023-03-07 00:23:00 +0800

    added grouBy fn for objArray with any key

--- common/utils.js
+++ common/utils.js
@@ -1,5 +1,7 @@
 const utils = {};
 
+utils.flaggedUsers = [1663882102141, 1663900040545, 1664485938220];
+
 utils.printProgress = (count, max) => {
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
@@ -10,6 +12,18 @@ utils.printProgress = (count, max) => {
 
 utils.formatPercent = (n) => {
   return (n * 100).toFixed(2) + "%";
+};
+
+utils.groupBy = (objArray, key) => {
+  const groups = {};
+  for (let obj of objArray) {
+    const val = obj[key];
+    if (groups[val] == null) {
+      groups[val] = [];
+    }
+    groups[val].push(obj);
+  }
+  return groups;
 };
 
 if (typeof module != "undefined") {
